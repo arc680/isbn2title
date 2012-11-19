a48c14dc4d5f8dbf00b9f9252a0779522bb67816
arc680 <maru.nslab@gmail.com>
2012-11-19 22:35:14 +0900

    やりたいことをやった

---
+++ isbn2title.rb
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+require 'open-uri'
+require 'json'
+require 'fileutils'
+
+def getisbn(filename)
+  re_str = /^oreilly-(978|979)-([0-9]+)-([0-9]+)-([0-9]+)-([0-9]{1})e.pdf$/
+  if re_str =~ filename
+    isbn = $1 + $2 + $3 + $4 + $5
+    if isbn.length == 13
+      return isbn
+    else
+      print("13桁ちゃうで\n")
+    end
+  else
+    print("フォーマット違うで.\n")
+  end
+end
+
+def getjson(isbn)
+  uri = "http://www.oreilly.co.jp/books/" + isbn + "/biblio.json"
+  json = ""
+  open(uri) do |f|
+    json += f.read
+  end
+  return JSON.parse(json)
+end
+
+def isbn2title(filename, new_filename)
+  FileUtils.cp(filename, "./renamed/" + new_filename)
+  print(filename + " -> " + new_filename + "\n")
+end
+
+
+if File.exists?("./renamed") == false
+  Dir.mkdir("./renamed")
+end
+
+files = Dir.glob("*.pdf")
+isbn = getisbn(files[0])
+if isbn != nil
+  json_data = getjson(isbn)
+  isbn2title(ARGV[0], json_data['title'] + ".pdf")
+end
